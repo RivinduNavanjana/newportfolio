3d1932f593fef45a2bdf85126e5cd566f105ed4f
RivinduNavanjana <ar98498@fhss.sjp.ac.lk>
2023-03-17 14:22:02 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,129 @@
+const logotext = "Rivindu";
+const meta = {
+    title: "Rivindu Navanjana",
+    description: "I’m Rivindu Navanjana data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Rivindu Navanjana",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Must possess technical skills and interpersonal skills and effectively communicate with users to test and debug software through to the end product.",
+    your_img_src: "src//assets//images//photo-1514790193030-c89d266d5a9d.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am an IT (Bsc)undergraduate at the University of Sri Jayewardenepura who is interested in UI/UX designing.Moreover, my passion is Music. So, when I have free time, I always love to play the violin which can fix my each and every mood less time.",
+    
+};
+const datacontact = {
+    title: "Contact Details",
+    aboutme1: "Rivindu Navanjana ",
+    aboutme2:"2/A Polhena, Madapatha,Piliyandala",
+    aboutme3: "0769 390 572",
+    aboutme4:"rivindunavanjana2000@gmail.com"
+    
+};
+
+
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+   
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "It's the extremely powerful art of creating visually pleasing, highly intuitive user experiences. As a UI designer, you'll work with a multidisciplinary team of designers, developers, and marketers to realize your projects",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Today, mobile devices—and the mobile applications that unlock their value—are the most popular way for people and businesses to connect to the internet. To stay relevant, responsive and successful, organizations need to develop the mobile applications that their customers, partners and employees demand.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Becoming a web designer allows you to use your creativity, following briefs and putting your own individual spin and style on them. Most projects you'll be given will have a particular deadline and task outline, but the reason you've been hired is because they require an expert to add that much needed creative flare.",
+    },
+];
+
+const dataportfolio = [{
+        img: "http://fhss.sjp.ac.lk/wp-content/uploads/2022/12/Frame-182.png",
+        desctiption: "We@FHSS is the latest mobile application of the Faculty of Humanities and Social Sciences, USJ. It enables undergraduates as well as the academic members of the faculty to experience following features.",
+        link: "http://fhss.sjp.ac.lk/mobile/",
+    },
+    {
+        img: "https://d33wubrfki0l68.cloudfront.net/432241ca94be9902c4d92aef279294e0d8ac81d4/4c9bb/images/dam/dam-illustration-1.webp",
+        desctiption: "The assest managment mobilr app",
+        link: "https://github.com/FHSS-USJ/end-semester-exam-Group-1.git",
+    },
+    {
+        img: "https://images.pexels.com/photos/6132745/pexels-photo-6132745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        desctiption: "The ATM system",
+        link: "https://github.com/RivinduNavanjana/ATM_System.git",
+    },
+    {
+        img: "https://scontent.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/254120385_100125122493318_9087091057793878998_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFPGLIxmbNyT-14cQKdKii5f2riwVZpjcZ_auLBVmmNxt1PZ-EuYYRxOlgiEuLRjVRug534SJgeaB3H4CAN4pxh&_nc_ohc=D1qs07AZ8jUAX-yQfIn&_nc_ht=scontent.fmaa8-1.fna&oh=00_AfCvU7zDeHMRGzF8NwK5u3POWVGsUn6jNwN4M71jqDvMew&oe=6417A3FF",
+        desctiption: "DON & CRYSSEL PIZZA web system",
+        link: "https://github.com/RivinduNavanjana/don-crytsel_pizza_web.git",
+    },
+    {
+        img: "https://images.pexels.com/photos/2873486/pexels-photo-2873486.jpeg?auto=compress&cs=tinysrgb&w=600",
+        desctiption: "The Movie select web system",
+        link: "https://github.com/RivinduNavanjana/Film_website.git",
+    },
+    {
+        img: "https://images.pexels.com/photos/7089047/pexels-photo-7089047.jpeg?auto=compress&cs=tinysrgb&w=600",
+        desctiption: "The Hospital managment system",
+        link: "https://github.com/RivinduNavanjana/care_hospital_system.git",
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rivindunavanjana2000@gmail.com",
+    YOUR_FONE: "076 939 0572",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_2de0dav",
+    YOUR_TEMPLATE_ID: "template_7pqunaz",
+    YOUR_USER_ID: "4PiQnVgf6T5OlZ0ok",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    datacontact,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
